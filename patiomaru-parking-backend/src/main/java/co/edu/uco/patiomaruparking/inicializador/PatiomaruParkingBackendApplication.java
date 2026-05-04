1350e71a11b6c4fc6fb5663c8b379ed4812b445b
yeisonvargas01 <yeisonvargass27@gmail.com>
2026-05-03 23:42:08 -0500

    primer_commit

---
+++ patiomaru-parking-backend/src/main/java/co/edu/uco/patiomaruparking/inicializador/PatiomaruParkingBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.patiomaruparking.inicializador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatiomaruParkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatiomaruParkingBackendApplication.class, args);
+	}
+
+}
---
+++ patiomaru-parking-backend/src/test/java/co/edu/uco/patiomaruparking/inicializador/PatiomaruParkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.patiomaruparking.inicializador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatiomaruParkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
