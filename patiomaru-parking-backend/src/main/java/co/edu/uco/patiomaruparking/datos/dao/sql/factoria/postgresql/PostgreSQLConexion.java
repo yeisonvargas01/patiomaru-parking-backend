f3e73fa7e3dd8c917eaf1c073874f3b5a45d5796
yeisonvargas01 <yeisonvargass27@gmail.com>
2026-05-29 15:58:15 -0500

    OK

--- patiomaru-parking-backend/src/main/java/co/edu/uco/patiomaruparking/datos/dao/sql/factoria/postgresql/PostgreSQLConexion.java
+++ patiomaru-parking-backend/src/main/java/co/edu/uco/patiomaruparking/datos/dao/sql/factoria/postgresql/PostgreSQLConexion.java
@@ -6,29 +6,24 @@ import java.sql.SQLException;
 
 public final class PostgreSQLConexion {
 
-    private static final String DRIVER = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://localhost:5433/patiomaru_parking";
-    private static final String USUARIO = "postgres";
-    private static final String CLAVE = "Ajstyles4#";
+	private static final String URL = "jdbc:postgresql://localhost:5432/patiomaru_db?currentSchema=restaurante_yeison";
+	private static final String USUARIO = "postgres";
+	private static final String CLAVE = "Ajstyles4#";
 
-    private PostgreSQLConexion() {
-        super();
-    }
+	private PostgreSQLConexion() {
+		super();
+	}
 
-    public static Connection obtenerConexion() {
-        try {
-            Class.forName(DRIVER);
-            return DriverManager.getConnection(URL, USUARIO, CLAVE);
+	public static Connection obtenerConexion() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			return DriverManager.getConnection(URL, USUARIO, CLAVE);
 
-        } catch (ClassNotFoundException excepcion) {
-            throw new RuntimeException(
-                    "No fue posible cargar el driver de PostgreSQL.",
-                    excepcion);
+		} catch (ClassNotFoundException excepcion) {
+			throw new RuntimeException("No fue posible cargar el driver de PostgreSQL.", excepcion);
 
-        } catch (SQLException excepcion) {
-            throw new RuntimeException(
-                    "No fue posible abrir la conexión con PostgreSQL.",
-                    excepcion);
-        }
-    }
+		} catch (SQLException excepcion) {
+			throw new RuntimeException("No fue posible abrir la conexión con PostgreSQL.", excepcion);
+		}
+	}
 }
